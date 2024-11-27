314234388a39f1553ec18fd6b21633b8d40274c8
hideki.tsuruoka.fb <hideki.tsuruoka.fb@nttdocomo.com>
2024-11-27 04:46:33 +0000

    市区町村一覧のUIを別ファイルにした

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/city_list_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,24 +13,6 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       title: 'Flutter RESAS App',
       home: CityListPage(),
-    );
-  }
-}
-
-class CityListPage extends StatelessWidget {
-  const CityListPage({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('市区町村一覧'),
-      ),
-      body: const Center(
-        child: Text('市区町村の一覧画面です'),
-      ),
     );
   }
 }
