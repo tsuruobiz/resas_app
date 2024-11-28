ae3334769f174d19157e7baf8293f12deac09368
hideki.tsuruoka.fb <hideki.tsuruoka.fb@nttdocomo.com>
2024-11-28 06:44:41 +0000

    詳細画面に一人あたりの地方税を表示

--- lib/city_detail_page.dart
+++ lib/city_detail_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -41,9 +43,15 @@ class _CityDetailPageState extends State<CityDetailPage> {
       body: FutureBuilder<String>(
         future: _future,
         builder: (context, snapshot) {
-          print(snapshot.data);
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          final result =
+              jsonDecode(snapshot.data!)['result'] as Map<String, dynamic>;
+          final data = result['data'] as List;
+          final items = data.cast<Map<String, dynamic>>();
           return Center(
-            child: Text('${widget.city}の詳細画面です'),
+            child: Text(items.toString()),
           );
         },
       ),
