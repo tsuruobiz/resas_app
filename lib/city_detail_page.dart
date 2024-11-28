1f498448a3e3d638bb120f567e31e0c6ad6202bd
hideki.tsuruoka.fb <hideki.tsuruoka.fb@nttdocomo.com>
2024-11-28 06:38:47 +0000

    一人当たり地方税のデータ取得を詳細画面を開いた時にできるようにした

--- lib/city_detail_page.dart
+++ lib/city_detail_page.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-class CityDetailPage extends StatelessWidget {
+import 'env.dart';
+
+class CityDetailPage extends StatefulWidget {
   const CityDetailPage({super.key, required this.city});
 
   final String city;
 
   @override
+  State<CityDetailPage> createState() => _CityDetailPageState();
+}
+
+class _CityDetailPageState extends State<CityDetailPage> {
+  late Future<String> _future;
+
+  @override
+  void initState() {
+    super.initState();
+    // APIからデータを取得する処理
+    const host = 'opendata.resas-portal.go.jp';
+    // 一人当たりの地方税を取得するエンドポイントを指定します
+    const endopoint = 'api/v1/municipality/taxes/perYear';
+    final headers = {'X-API-KEY': Env.resasApiKey};
+    final param = {
+      'prefCode': '14',
+      'cityCode': '14102',
+    };
+    _future = http
+        .get(Uri.https(host, endopoint, param), headers: headers)
+        .then((res) => res.body);
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(city),
+        title: Text(widget.city),
       ),
-      body: Center(
-        child: Text('$cityの詳細画面です'),
+      body: FutureBuilder<String>(
+        future: _future,
+        builder: (context, snapshot) {
+          print(snapshot.data);
+          return Center(
+            child: Text('${widget.city}の詳細画面です'),
+          );
+        },
       ),
     );
   }
